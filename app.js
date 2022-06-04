436b79c591d4881d5a2b001b1aaebb1d8272d275
Abhinav Mishra <abhinavm65@gmail.com>
2022-06-04 23:46:53 +0530

    initail commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// // Built in Module
+// // OS Module
+// const os = require('os')
+// const path = require('path')
+
+// // info about current user
+// const user = os.userInfo()
+// console.log(user);
+
+// // returns system uptime in sec
+// console.log('uptime',os.uptime()); 
+
+// // console.log(path.sep);
+
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+ res.write('Welcome to homepage')
+ res.end()
+});
+
+server.listen(3001);
---
+++ module.js
@@ -0,0 +1,12 @@
+//local
+const secret = 'SECRET';
+//shared
+const john = 'john';
+const peter = 'peter';
+
+console.log(module);
+
+module.exports = {
+    john,
+    peter
+}
